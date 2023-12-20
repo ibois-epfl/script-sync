4e32c20cc5e17000edc0045807b90352c4d48046
Andrea Settimi <andreasettimi39@gmail.com>
2023-12-20 17:38:24 +0100

    CAP-WIP: working thread with modules

--- pyversion.py
+++ pyversion.py
@@ -5,10 +5,10 @@ import platform
 
 import Rhino
 
-# import module_imp
+import module_imp
 
 Rhino.RhinoApp.WriteLine(platform.python_implementation())
 Rhino.RhinoApp.WriteLine(sys.version)
 print("TESTING")
 
-# module_imp.test_module()
+module_imp.test_module()
