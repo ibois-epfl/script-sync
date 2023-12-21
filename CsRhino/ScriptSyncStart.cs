b47b208e9ac201454ceaa18b295e5affdccc4067
Andrea Settimi <andreasettimi39@gmail.com>
2023-12-21 22:52:00 +0100

    FIX: creating temproary fire test and non-blocking

--- CsRhino/ScriptSyncStart.cs
+++ CsRhino/ScriptSyncStart.cs
@@ -35,7 +35,6 @@ namespace ScriptSync
 
         protected override Rhino.Commands.Result RunCommand(RhinoDoc doc, RunMode mode)
         {
-            // start a local server
             RhinoApp.WriteLine("Starting ScriptSync..");
             if (IsRunning)
             {
@@ -56,25 +55,19 @@ namespace ScriptSync
             _server.Start();
             IsRunning = true;
 
-            // FIXME: this won't work in the shipped version 
             RhinoApp.InvokeOnUiThread(new Action(() =>
             {
                 if (!IsScriptEditorRunnerFromThreadOk())
-                    throw new Exception("ScriptEditorRunner is not working");
+                    RhinoApp.WriteLine("Warning: ScriptEditorRunner is failing starting tests");
             }));
 
             while (IsRunning)
             {
-
                 TcpClient client = _server.AcceptTcpClient();
                 byte[] data = new byte[1024];
                 NetworkStream stream = client.GetStream();
                 int bytesRead = stream.Read(data, 0, data.Length);
                 string scriptPath = Encoding.ASCII.GetString(data, 0, bytesRead);
-                RhinoApp.InvokeOnUiThread(new Action(() =>
-                {
-                    RhinoApp.WriteLine("Path. " + scriptPath + "");
-                }));
 
                 if (bytesRead == 0)
                 {
@@ -107,13 +100,21 @@ namespace ScriptSync
 
         private bool IsScriptEditorRunnerFromThreadOk()
         {
-            string cPyScriptPath = System.IO.Path.GetFullPath(@"./tests/cpy_version.py");
-            string ironPyScriptPath = System.IO.Path.GetFullPath(@"./tests/ironpy_version.py");
-            string csScriptPath = System.IO.Path.GetFullPath(@"./tests/CsVersion.cs");
+            string cPyScriptPath = System.IO.Path.GetFullPath(@"./temp/cpy_version.py");
+            string ironPyScriptPath = System.IO.Path.GetFullPath(@"./temp/ironpy_version.py");
+            string csScriptPath = System.IO.Path.GetFullPath(@"./temp/CsVersion.cs");
+
+            System.IO.File.WriteAllText(cPyScriptPath, "#! python3\nimport sys\nprint(sys.version)");
+            System.IO.File.WriteAllText(ironPyScriptPath, "#! python2\nimport sys\nprint(sys.version)");
+            System.IO.File.WriteAllText(csScriptPath, "using System;\n\nCsVersion.Main();\n\nclass CsVersion\n{\n\tstatic public void Main()\n\t{\n\t\tConsole.WriteLine(\"C# Runtime: \" + Environment.Version.ToString());\n\t\tConsole.WriteLine(\"platform: \" + Environment.OSVersion.ToString());\n\t}\n}");
 
             bool cPyIsRunning = RhinoApp.RunScript("_-ScriptEditor Run " + cPyScriptPath, false);
             bool ironPyIsRunning = RhinoApp.RunScript("_-ScriptEditor Run " + ironPyScriptPath, false);
             bool csIsRunning = RhinoApp.RunScript("_-ScriptEditor Run " + csScriptPath, false);
+
+            System.IO.File.Delete(cPyScriptPath);
+            System.IO.File.Delete(ironPyScriptPath);
+            System.IO.File.Delete(csScriptPath);
 
             if (!cPyIsRunning || !ironPyIsRunning || !csIsRunning)
                 return false;
