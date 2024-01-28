4dd1b1994550ad25cb18646236d602552c6aab74
Andrea Settimi <andreasettimi39@gmail.com>
2024-01-28 22:26:51 +0100

    CAP-WIP: modifiedd functioning CI componentizer

--- GH/PyGH/test/runner_script.py
+++ GH/PyGH/test/runner_script.py
@@ -1,8 +1,11 @@
 #! python3
 
+x = 123
+y = 456
+a = x - y
+c = x + y
+b = 123456
+
 print(f"runner_script.py::x value: {x}")
 print(f"runner_script.py::y value: {y}")
-a = 422
-c = x + y
-b = 1239
 print(f"runner_script.py::a value: {a}")
